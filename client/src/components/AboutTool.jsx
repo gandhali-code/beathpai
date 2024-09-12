5831213e7a299a835ab2926cac986ab19c28ce50
gandhali-code <gandhalikokate24@gmail.com>
2024-09-12 11:19:59 -0400

    Update AboutTool.jsx

--- client/src/components/AboutTool.jsx
+++ client/src/components/AboutTool.jsx
@@ -13,6 +13,8 @@ function Abouttool() {
 The BEAT-HPAI tool is based on the 14 biosecurity principles recommended by the National Poultry Improvement Plan (NPIP). By simply answering a short survey, you can receive a clear score of your compliance with these principles. More importantly, the tool will provide you with detailed corrective actions tailored to address any gaps in your current biosecurity plan.
 
         </p>
+        <br></br>
+        
         <p className="about-tool-description">
         <h1 className="about-tool-title">How to use the tool</h1>
         <strong>Access the Survey</strong>: Start by accessing the BEAT-HPAI survey on our website. The survey consists of biosecurity-related questions covering all aspects of your farm operations. 
@@ -23,6 +25,7 @@ The BEAT-HPAI tool is based on the 14 biosecurity principles recommended by the 
 <strong>Receive Your Score</strong>: Once you have completed the survey, the tool will generate a score that reflects your farm’s compliance with NPIP biosecurity principles.
 <br></br>
 <strong>Review Corrective Actions</strong>: Along with your score, you’ll receive a detailed list of corrective measures that you can implement to strengthen your biosecurity plan. These recommendations are customized based on your responses to ensure they are relevant and actionable. 
+<br></br>
 <br></br>
         </p>
     </div>
@@ -39,11 +42,13 @@ The BEAT-HPAI tool is based on the 14 biosecurity principles recommended by the 
 <br></br>
 <strong>Privacy Protection: </strong>We respect your privacy. The BEAT-HPAI tool does not collect or store any of your answers, scores, or farm identification information. 
 <br></br>
+<br></br>
        </p>
 
        <h1 className="about-tool-title">Get Started Today!</h1>
        <p className="about-tool-description">
        We encourage all poultry growers to take advantage of this valuable tool. Assess your current biosecurity plan, make the necessary improvements, and protect your farm from the devastating impacts of HPAI. Your commitment to biosecurity is not just about compliance; it's about safeguarding your livelihood and the broader poultry industry.</p>
+       <br></br>
        <h1 className="about-tool-title">Meet the Team behind BEAT-HPAI</h1>
        <p className="about-tool-description">
        <strong>BEAT-HPAI </strong>was developed by Dr. Mostafa Ghanem in collaboration with graduate student Madhusudan Timilsina, with web support from Gandhali Kokate and inputs from Dr. Nathaniel Tablante. This project was funded by a USDA-NIFA Smith-Lever Special Needs grant, supporting the University of Maryland Poultry Extension Team, which includes Dr. Ghanem, Dr. Tablante, Dr. Jon Moyle, Jenny Rhodes, and Maegan Perdue.
@@ -58,6 +63,7 @@ The BEAT-HPAI tool is based on the 14 biosecurity principles recommended by the 
         </Link>
     </div>
 </div>
+
 
     );
 }
